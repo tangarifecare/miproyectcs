70d4cb2187afa6b2a0db715887534fc5cb632b29
DanielFelipeAgMo <danielagudelo87@gmail.com>
2022-03-13 20:54:05 -0500

    Creación del proyecto JAVA en Netbeas y SQLite

---
+++ sistema/src/sistema/Sistema.java
@@ -0,0 +1,20 @@
+/*
+ Creación del paquete sistema
+ */
+package sistema;
+
+/**
+ * Se implementa la clase sistema
+ * @author Daniel
+ */
+public class Sistema {
+
+    /**
+     * Argumentos generados a la clase principal por parte de netbeans
+     * @param args the command 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
